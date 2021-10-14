11751b9d853021adced4d87ac905e51555ffb76b
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2021-10-14 15:21:45 +0100

    Add flag -l. Corrigi alguns erros na contagem das colunas. Ainda n tem a contagem das colunas a 100%

--- src/teste.py
+++ src/teste.py
@@ -7,8 +7,12 @@ e coloca os resultados na pasta outputs com o devido nome
 import os
 
 test_files = []
+failed = 0
 
-os.system("mkdir meta1/outputs")
+if not os.path.exists("meta1/outputs"):
+    print("Created path")
+    os.system("mkdir meta1/outputs")
+
 
 for test in os.listdir('./meta1'):
     if '.dgo' in test:
@@ -16,5 +20,12 @@ for test in os.listdir('./meta1'):
 
 
 for test in test_files:
-    os.system(f'make && ./gocompiler < meta1/{test}.dgo | diff meta1/{test}.out - > meta1/outputs/{test}.txt')
+    os.system(f'make && ./gocompiler -l < meta1/{test}.dgo | diff meta1/{test}.out - > meta1/outputs/{test}.txt')
+    
+    if os.stat(f"meta1/outputs/{test}.txt").st_size != 0:
+        failed += 1
+        print(f"{test}.dgo -> Failed")
+    
 
+if failed == 0:
+    print("No Fails!")
