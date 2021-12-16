caa80460b17d96af122f327ab79c00f63b15d9ac
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2021-12-16 22:36:11 +0000

    fix local variable not declared

--- src/dgo.sh
+++ src/dgo.sh
@@ -1,5 +1,5 @@
 # Usage: ./dgo.sh [file] [args]
 
 make && ./gocompiler < $1 > dgo # compile
-lli dgo $2 $3 $4 $5 $6 # run. up to 5 arguments
+lli-7 dgo $2 $3 $4 $5 $6 # run. up to 5 arguments
 rm dgo # delete
